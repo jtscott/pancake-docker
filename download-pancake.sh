759546956157a8b21d80922a1689b6e922830e5b
jtscott <jtscott@docker.riffz.ca>
2016-11-28 09:12:51 -0700

    fix for auto update

--- download-pancake.sh
+++ download-pancake.sh
@@ -4,5 +4,6 @@ curl http://manage.pancakeapp.com/download/$1 > pancake.zip \
 && mv pancake_4/pancake/* /var/www/html \
 && chmod -R 777 /var/www/html/system/pancake \
 && chmod -R 777 /var/www/html/uploads \
+&& chown -R www-data:www-data /var/www/html \
 && rm pancake.zip \
 && rm -rf pancake_4/
